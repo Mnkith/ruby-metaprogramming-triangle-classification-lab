9af6d7cd3129a4de4ffd626de73d4a5cd6f7a2a4
Mnkith <munkithabid@gmail.com>
2020-11-20 13:14:02 -0600

    commit tree ee7ad319fbd3a3f324c9fbd8f0db16cd7615f505

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  def initialize(s1, s2, s3)
+    @a, @b, @c = s1, s2, s3 
+  end
+
+  def kind
+    if @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a != @b && @b != @c && @a != @c
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
